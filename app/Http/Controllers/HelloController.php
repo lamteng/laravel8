f668aead7815b4b02e257b97a8c4c2c9228a6001
Lam Teng <lamteng@hotmail.com>
2021-03-12 22:49:03 +0000

    added new blade file

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,13 +8,8 @@ use Illuminate\Http\Request;
 class HelloController extends Controller
 {
     public function index(){
-        $coolString = "Hello from Routes AGAIN.";
 
-        return view('subviews.hello', 
-         
-            compact('coolString') 
-        
-        );
+        return view('about');
     
     }
 }
---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('app')
+
+@section('title', 'About Us Page')
+
+@section('content')
+    <h1>Welcome to Laravel 8 Content </h1>
+
+    <p>with another content text.</p>
+@endsection
